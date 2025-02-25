cd7924b461c8746c6dbdd736f293878f7188199f
Haagphur <52178892+Megamax21@users.noreply.github.com>
2025-02-25 17:58:01 +0100

    Add files via upload

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: ml-hote <ml-hote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 16:56:04 by ml-hote           #+#    #+#             */
-/*   Updated: 2025/02/22 18:02:57 by ml-hote          ###   ########.fr       */
+/*   Updated: 2025/02/25 16:33:53 by ml-hote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		main(int arg_c, char **arg_v)
 	int		nb_args;
 
 	nb_args = 1;
-	printf("Mon programme prend %i arguments \n", arg_c - 1);
+	printf("\nMon programme prend %i arguments \n", arg_c - 1);
 	while (nb_args < arg_c)
 	{
 		printf("arg %i : %s\n", nb_args, arg_v[nb_args]);
@@ -35,13 +35,28 @@ int		main(int arg_c, char **arg_v)
 
 	/*	Tests sur listes chainees	*/
 	printf("Comment ca marche les listes chainees ?\n");
-	l1 = ft_lstnew((void *)21);
-	l2 = ft_lstnew((void *)22);
-	printf("On cree deux variables !\n");
-	
-	//printf("Test valeur : %i", l1->content);
-	ft_lstadd_back(&l1, l2);
-	printf_list(l1);
+	l2 = l1;
+
+	/* Remplissage de la pile a */
+	if (arg_c > 1)
+	{
+		printf("---\n");
+		l1 = ft_lstnew(ft_atoi(arg_v[1])); // On donne a ma liste chainee sa toute premiere valeur
+		printf("===\n");
+		if (arg_c > 2)
+		{
+			nb_args = 2;
+			while (nb_args < arg_c)
+			{
+				ft_lstadd_back(&l1, ft_lstnew(ft_atoi(arg_v[nb_args]))); // A chaque fois qu'on trouve un nouvel argument on le rajoute au tout debut de la liste
+				nb_args++;
+			}
+		}
+		
+
+		printf("Arguments rajoutes a la liste\n");
+		printf_list(l1);	
+	}
 	ft_putstr_fd("test\n", 1);
 	return  (0);
 }
