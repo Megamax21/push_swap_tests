677d4d00f0ad6a477535af85a3dc284def8774d0
Haagphur <52178892+Megamax21@users.noreply.github.com>
2025-03-04 13:43:58 +0100

    Add files via upload

---
+++ libft/ft_lstnew_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ml-hote <ml-hote@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 10:34:06 by ml-hote           #+#    #+#             */
+/*   Updated: 2025/02/22 23:41:55 by ml-hote          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(int content)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
