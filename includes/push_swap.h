2d056980d7696fb5befc58fef382c07cdc3a2c82
Haagphur <52178892+Megamax21@users.noreply.github.com>
2025-03-14 15:41:04 +0100

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,1 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ml-hote <ml-hote@student.42nice.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 16:57:06 by ml-hote           #+#    #+#             */
+/*   Updated: 2025/03/12 09:19:19 by ml-hote          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdint.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "../libft/libft.h"
+
+/* TEST FUNCTIONS */
+
+/* Returns an int that represents the length of the linked list
+	an takes a linked list as the l argument */
+int		ft_print_list(t_list *l, char name);
+int		ft_check_sorted(t_list *l);
+int		ft_check_content(char *value);
+int		ft_check_double(int val, t_list *l);
+int		ft_find_min();
+int		ft_fill_stack(int len, char **entries,t_list **stack);
+void	ft_swap(t_list *l, char name);
+void	ft_push(t_list **from, t_list **to, char name_to);
+void	ft_rotate(t_list **l, char name);
+void	ft_rev_rot(t_list **l, char name);
+void	ft_ss(t_list *list_a, t_list *list_b);
+void	ft_rr(t_list *list_a, t_list *list_b);
+void	ft_rrr(t_list *list_a, t_list *list_b);
+void	ft_print_rule(char* rule, char stack);
+
+#endif
