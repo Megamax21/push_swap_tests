16b2f9d41298ab954ac0aeeccc54aac5f6353b4e
Haagphur <52178892+Megamax21@users.noreply.github.com>
2025-03-22 18:06:49 +0100

    Add files via upload

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ml-hote <ml-hote@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 16:57:06 by ml-hote           #+#    #+#             */
-/*   Updated: 2025/03/12 09:19:19 by ml-hote          ###   ########.fr       */
+/*   Updated: 2025/03/17 22:57:11 by ml-hote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,25 @@
 /* Returns an int that represents the length of the linked list
 	an takes a linked list as the l argument */
 int		ft_print_list(t_list *l, char name);
-int		ft_check_sorted(t_list *l);
+int		ft_check_sorted(t_list **l);
 int		ft_check_content(char *value);
 int		ft_check_double(int val, t_list *l);
-int		ft_find_min();
+int		ft_find_min(t_list **l);
+int		ft_find_max(t_list **l);
+int		ft_check_order(t_list *l);
 int		ft_fill_stack(int len, char **entries,t_list **stack);
-void	ft_swap(t_list *l, char name);
+int		ft_find_next_min(int ind, int min, t_list **l);
+void	ft_swap(t_list **l, char name);
 void	ft_push(t_list **from, t_list **to, char name_to);
 void	ft_rotate(t_list **l, char name);
 void	ft_rev_rot(t_list **l, char name);
-void	ft_ss(t_list *list_a, t_list *list_b);
+void	ft_ss(t_list **list_a, t_list **list_b);
 void	ft_rr(t_list *list_a, t_list *list_b);
 void	ft_rrr(t_list *list_a, t_list *list_b);
 void	ft_print_rule(char* rule, char stack);
+void	ft_assign_index(t_list **l);
+void	ft_sort_two(t_list **l);
+void	ft_sort(t_list **entries);
+void	ft_sort_three(t_list **l);
 
 #endif
